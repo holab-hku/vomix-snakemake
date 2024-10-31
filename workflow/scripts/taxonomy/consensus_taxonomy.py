44be19079ed068039956735296653cfebd557565
Erfan SHEKARRIZ <eshekar@xomics.cgs.hku.hk>
2024-10-31 14:58:35 +0800

    updated cache

--- workflow/scripts/taxonomy/consensus_taxonomy.py
+++
@@ -1,8 +0,0 @@
-taxcols = ['species', 'genus', 'family', 'order', 'class', 'phylum', 'kingdom']
-
-scoredict = {}
-scoredict = {key: {} for key in headers}
-
-for key in scoredict:
-	for taxlev in taxcols:
-		scoredict[taxlev] = set(mergedf.filter(like=taxlev).values.flatten())
