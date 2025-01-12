b2b62052a6941575f3d8cd792dc760d0ea33f99f
Erfan SHEKARRIZ <eshekar@xomics.cgs.hku.hk>
2025-01-12 20:07:13 +0800

    [Bug] update_latest_vomix.bash now has smarter cmp comparisons

--- workflow/scripts/update_latest_vomix.bash
+++ workflow/scripts/update_latest_vomix.bash
@@ -5,6 +5,7 @@ PWD=$(pwd)
 DIR=$1
 DIR_CLEAN="${PWD}/${DIR%/}"
 
+rm -rf vomix-tmp
 
 echo "Cloning repository $REPO..."
 git clone "https://github.com/$REPO.git" vomix-tmp
@@ -14,26 +15,96 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# 1) Snakefile
 echo "[1/5] Updating Snakefile..."
 oldfile="${DIR_CLEAN}/workflow/Snakefile"
 newfile="vomix-tmp/workflow/Snakefile"
-if cmp -s $oldfile $newfile; then : ; else echo "Updating Snakefile"; mv $oldfile $newfile; fi
+cmp -s "$oldfile" "$newfile"
+case $? in
+    0) 
+        # Do nothing if cmp is successful
+        ;;
+    1 | 2) 
+        echo "Updating Snakefile"
+        mv "$oldfile" "$newfile"
+        ;;
+esac
 
+
+# 2) config.yml
 echo "[2/5] Updating config.yml..."
 oldfile="${DIR_CLEAN}/config/config.yml"
 newfile="vomix-tmp/config/config.yml"
-if [[ -e $oldfile; then cp $oldfile "${original_file%.yml}_old.yml"; else : ;fi
-if cmp -s $oldfile $newfile; then : ; else echo "Updating config.yml"; mv $oldfile $newfile; fi
+if [[ -e "$oldfile" ]]; then 
+    cp "$oldfile" "${original_file%.yml}_old.yml"
+else 
+    : 
+fi
+cmp -s "$oldfile" "$newfile"
+case $? in
+    0) 
+        # Do nothing if cmp is successful
+        ;;
+    1 | 2) 
+        echo "Updating Snakefile"
+        mv "$oldfile" "$newfile"
+        ;;
+esac
 
+# 3) Rules
 echo "[3/5] Updating rules..." 
-for newfile in $(find vomix-tmp/workflow/rules/ -maxdepth 1 -type f); do BASE=$(basename ${newfile}); oldfile="${DIR_CLEAN}/workflow/rules/${BASE}"; if cmp -s "$oldfile" "$newfile"; then : ; else echo "Updating $BASE"; mv $oldfile $newfile; fi; done
+for newfile in $(find vomix-tmp/workflow/rules/ -maxdepth 1 -type f); do
+    BASE=$(basename "$newfile")
+    oldfile="${DIR_CLEAN}/workflow/rules/${BASE}"
+    
+    cmp -s "$oldfile" "$newfile"
+    case $? in
+        0) 
+            # Do nothing if cmp is successful
+            ;;
+        1 | 2) 
+            echo "Updating $BASE"
+            mv "$oldfile" "$newfile"
+            ;;
+    esac
+done
 
+# 4) Environments
 echo "[4/5] Updating envs..." 
-for newfile in $(find vomix-tmp/workflow/envs/ -maxdepth 1 -type f); do BASE=$(basename ${newfile}); oldfile="${DIR_CLEAN}/workflow/envs/${BASE}"; if cmp -s "$oldfile" "$newfile"; then : ; else echo "Updating $BASE"; mv $oldfile $newfile; fi;  done
+for newfile in $(find vomix-tmp/workflow/envs/ -maxdepth 1 -type f); do
+    BASE=$(basename "$newfile")
+    oldfile="${DIR_CLEAN}/workflow/envs/${BASE}"
 
+    cmp -s "$oldfile" "$newfile"
+    case $? in
+        0)
+            # Do nothing if cmp is successful
+            ;;
+        1 | 2)
+            echo "Updating $BASE"
+            mv "$oldfile" "$newfile"
+            ;;
+    esac
+done
+
+# 5) Scripts
 echo "[5/5] Updating scripts..." 
-for newfile in $(find vomix-tmp/workflow/scripts/ -maxdepth 1 -type f); do BASE=$(basename ${newfile}); oldfile="${DIR_CLEAN}/workflow/scripts/${BASE}"; if cmp -s "$oldfile" "$newfile"; then : ; else echo "Updating $BASE"; mv $oldfile $newfile; fi; done
+for newfile in $(find vomix-tmp/workflow/scripts/ -maxdepth 1 -type f); do
+    BASE=$(basename "$newfile")
+    oldfile="${DIR_CLEAN}/workflow/scripts/${BASE}"
+
+    cmp -s "$oldfile" "$newfile"
+    case $? in
+        0)
+            # Do nothing if cmp is successful
+            ;;
+        1 | 2)
+            echo "Updating $BASE"
+            mv "$oldfile" "$newfile"
+            ;;
+    esac
+done
 
 echo "Done!" 
 
-rm -r 
+rm -r vomix-tmp
